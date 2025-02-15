bfc99037546cb99dc7241b4373889e25b1ee9e40
ambushura <gumbertep@gmail.com>
2025-02-15 15:35:50 +0300

    start

--- src/page/pages/schedule/PageSchedule.jsx
+++ src/page/pages/schedule/PageSchedule.jsx
@@ -1,8 +1,8 @@
 import {useSetSchedule} from "../../../hooks/useSetSchedule.js"
-import {Box} from "@mui/material"
+import {Box, Button} from "@mui/material"
 import SeanceCard from "./SeanceCard.jsx"
 import {useDispatch, useSelector} from "react-redux"
-import {useEffect} from "react"
+import {useEffect, useRef, useState} from "react"
 import {setSchedule} from "../../../redux/scheduleReducer.js"
 import ScheduleMenu from "../../../components/cinema/ScheduleMenu.jsx"
 import {useSetCurrentPage} from "../../../hooks/useSetCurrentPage.js"
@@ -34,6 +34,15 @@ const PageSchedule = () => {
         }
     }, [dispatch, filial_halls_seances])
 
+    // Массив ширины контента по филиалам
+    const elementsRef = useRef(new Map())
+    // Максимальная ширина для перекрытия фона при прокрутке
+    const [content_width, set_content_width] = useState(0)
+    useEffect(() => {
+        const widths = Array.from(elementsRef.current.values()).map(el => el?.getBoundingClientRect().width || 0)
+        set_content_width(Math.max(...widths))
+    }, [schedule])
+
     return (
         <>
             <ScheduleMenu/>
@@ -47,22 +56,22 @@ const PageSchedule = () => {
                         <Box id="content"
                              sx={{height: content_height}}>
                             <Box id="schedule-full">
-                                {schedule.map(filial_hall_seances => {
+                                {schedule.map((filial_hall_seances, index) => {
                                     if (filial_hall_seances.error !== null) {
-                                        return (
-                                            <Box key={filial_hall_seances.filial.uid}>
-                                                <Box className="schedule-full-filial-name">
-                                                    <span>{filial_hall_seances.filial.name}</span>
-                                                </Box>
-                                                <Box className="schedule-full-filial">
-                                                    Ошибка загрузки {filial_hall_seances.error}
-                                                </Box>
-                                            </Box>)
+                                        return (<></>)
                                     } else if (filial_hall_seances.loading) {
                                         return (
                                             <Box key={filial_hall_seances.filial.uid}>
-                                                <Box className="schedule-full-filial-name">
-                                                    <span>{filial_hall_seances.filial.name}</span>
+                                                <Box className="schedule-full-filial-name" style={{
+                                                    position: 'sticky',
+                                                    left: 0,
+                                                    top: 0,
+                                                    zIndex: 100,
+                                                    paddingLeft: '15px',
+                                                    width: `${content_width}px`
+                                                }}>
+                                                    <Button variant='contained' color='primary'
+                                                            style={{marginBottom: '5px'}}>{filial_hall_seances.filial.name}</Button>
                                                 </Box>
                                                 <Box className="schedule-full-filial">
                                                     <Loader key={filial_hall_seances.filial.uid}/>
@@ -73,17 +82,36 @@ const PageSchedule = () => {
                                         return (
                                             <Box key={filial_hall_seances.filial.uid}>
                                                 <Box
-                                                    className="schedule-full-filial-name">
-                                                    <span>{filial_hall_seances.filial.name}</span>
+                                                    className="schedule-full-filial-name" style={{
+                                                    position: 'sticky',
+                                                    left: 0,
+                                                    top: 0,
+                                                    zIndex: 100,
+                                                    paddingLeft: '15px',
+                                                    width: `${content_width}px`
+                                                }}>
+                                                    <Button variant='contained'
+                                                            color='primary'>{filial_hall_seances.filial.name}</Button>
                                                 </Box>
-                                                <Box className="schedule-full-filial">
+                                                <Box className="schedule-full-filial"
+                                                     ref={el => elementsRef.current.set(index, el)}>
                                                     {filial_hall_seances.data.map(hall => {
                                                         return (
                                                             <Box key={hall.uid} className='schedule-full-hall'>
-                                                                <Box className='schedule-full-hall-name'>
-                                                                    <Box>Зал {hall.name_full_hall}</Box>
+                                                                <Box className='schedule-full-hall-name' style={{
+                                                                    position: 'sticky',
+                                                                    top: '65px',
+                                                                    zIndex: 99,
+                                                                    marginBottom: '5px'
+                                                                }}>
+                                                                    <Button variant='contained' color='primary'
+                                                                            style={{width: '100%'}}>Зал {hall.name_full_hall}</Button>
                                                                 </Box>
-                                                                <Box className='schedule-full-seances'>
+                                                                <Box className='schedule-full-seances' style={{
+                                                                    position: 'sticky',
+                                                                    top: '130px',
+                                                                    zIndex: 98,
+                                                                }}>
                                                                     {hall.seances.map(seance => {
                                                                         return (
                                                                             <SeanceCard
