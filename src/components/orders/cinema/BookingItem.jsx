f927d4b82b5d26e226ce7736d2d009e0c9a69f8b
ambushura <gumbertep@gmail.com>
2025-05-06 11:59:31 +0300

    start

--- src/components/orders/cinema/BookingItem.jsx
+++ src/components/orders/cinema/BookingItem.jsx
@@ -10,20 +10,22 @@ const BookingItem = (props) => {
     return (
         <Box className="order-booking-item">
             <Box
-                className={`order-booking-item-description ${props.uid_selected.find(el => el === props.uid) ? 'position-selected' : ''}`}
-                onClick={() => {
+                className={`order-booking-item-description ${props.uid_selected.find(el => el === props.uid) ? 'position-selected' : ''}`}>
+                <div style={{height:'100%', display: 'flex', justifyContent: 'center', alignItems: 'center', width: 'calc(100% - 50px)'}}
+                     onClick={() => {
                     props.set_uid_selected(prev =>
                         prev.includes(props.uid)
                             ? prev.filter(el => el !== props.uid)
                             : [...prev, props.uid]
                     )
                 }}>
-                <span className="order-booking-item-numbers">{props.place_row}</span>
-                <span className="order-booking-item-labels">р</span>
-                <span className="order-booking-item-numbers">{props.place_number}</span>
-                <span className="order-booking-item-labels">м</span>
-                <span className="order-booking-item-numbers">{props.price}</span>
-                <span className="order-booking-item-labels">р</span>
+                    <span className="order-booking-item-numbers">{props.place_row}</span>
+                    <span className="order-booking-item-labels">р</span>
+                    <span className="order-booking-item-numbers">{props.place_number}</span>
+                    <span className="order-booking-item-labels">м</span>
+                    <span className="order-booking-item-numbers">{props.price}</span>
+                    <span className="order-booking-item-labels">р</span>
+                </div>
                 <button className='order-booking-item-button' onClick={() => {
                     dispatch(openModal({type: 'comment', props: {}}))
                 }}><BorderColorIcon/></button>
