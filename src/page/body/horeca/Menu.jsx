2969046d7a25416b7822086e1de4444509d1ce4b
ambushura <gumbertep@gmail.com>
2025-06-01 14:53:00 +0300

    start

--- src/page/body/horeca/Menu.jsx
+++ src/page/body/horeca/Menu.jsx
@@ -3,6 +3,7 @@ import {useSetMenu} from "../../../hooks/interface/useSetMenu.js"
 import {useEffect, useState} from "react"
 import Folder from "./Folder.jsx"
 import Item from "./Item.jsx"
+import {AnimatePresence, motion} from 'framer-motion'
 
 const Menu = () => {
 
@@ -15,49 +16,88 @@ const Menu = () => {
         const breadcrumbs_new = []
         for (let item = menu_data.breadcrumbs; item; item = item.folder) {
             breadcrumbs_new.unshift(
-                <Folder
-                    type="menu-breadcrumb"
-                    set_uid_folder={set_uid_folder}
-                    item={item}
-                    key={item.uid}/>
+                <motion.div
+                    key={item.uid}
+                    variants={itemVariants}>
+                    <Folder
+                        type="menu-breadcrumb"
+                        set_uid_folder={set_uid_folder}
+                        item={item}/>
+                </motion.div>
             )
         }
         set_breadcrumbs(breadcrumbs_new)
     }, [menu_data])
 
-    return (
-        <Box id="horeca-menu">
-            {menu_data !== undefined ?
-                <>
-                    <Box className='menu-breadcrumbs'>{breadcrumbs}</Box>
-                    <Box className='menu-folders'>
-                        {menu_data.items.map(item => {
-                            if (item.its_folder) {
-                                return (
-                                    <Folder
-                                        type="menu-folder"
-                                        set_uid_folder={set_uid_folder}
-                                        item={item}
-                                        key={item.uid}/>
-                                )
-                            }
-                        })}
-                    </Box>
-                    <Box className='menu-items'>
-                        {menu_data.items.map(item => {
-                            if (!item.its_folder) {
-                                return (
-                                    <Item
-                                        item={item}
-                                        key={item.uid}/>
-                                )
-                            }
-                        })}
-                    </Box>
-                </> : <></>
-            }
-        </Box>
-    )
+    return (<Box id="horeca-menu">
+        {menu_data !== undefined ? <>
+            <AnimatePresence>
+                {breadcrumbs.length > 0 && <motion.div
+                    className='menu-breadcrumbs'
+                    initial="hidden"
+                    animate="visible"
+                    exit="hidden"
+                    variants={containerVariants}>{breadcrumbs}</motion.div>}
+            </AnimatePresence>
+            <AnimatePresence>
+                {menu_data.items.length > 0 && <motion.div
+                    className='menu-folders'
+                    initial="hidden"
+                    animate="visible"
+                    exit="hidden"
+                    variants={containerVariants}>
+                    {menu_data.items.map(item => {
+                        if (item.its_folder) {
+                            return (<motion.div
+                                key={item.uid}
+                                variants={itemVariants}>
+                                <Folder
+                                    type="menu-folder"
+                                    set_uid_folder={set_uid_folder}
+                                    item={item}/>
+                            </motion.div>)
+                        }
+                    })}
+                </motion.div>}
+            </AnimatePresence>
+            <AnimatePresence>
+                {menu_data.items.length > 0 && <motion.div
+                    className='menu-items'
+                    initial="hidden"
+                    animate="visible"
+                    exit="hidden"
+                    variants={containerVariants}>
+                    {menu_data.items.map(item => {
+                        if (!item.its_folder) {
+                            return (
+                                <motion.div
+                                    key={item.uid}
+                                    variants={itemVariants}>
+                                    <Item item={item}/>
+                                </motion.div>
+                            )
+                        }
+                    })}
+                </motion.div>}
+            </AnimatePresence>
+        </> : <></>}
+    </Box>)
 }
 
 export default Menu
+
+const containerVariants = {
+    hidden: {}, visible: {
+        transition: {
+            staggerChildren: 0.2, delayChildren: 0.1
+        }
+    }
+}
+
+const itemVariants = {
+    hidden: {opacity: 0, x: -10}, visible: {
+        opacity: 1, x: 0, transition: {
+            duration: 0.4, ease: "easeOut"
+        }
+    }
+}
