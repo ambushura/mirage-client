71d54027f289b5e80723739363d3571dd60017aa
ambushura <gumbertep@gmail.com>
2025-06-18 18:17:28 +0300

    start

--- src/hooks/common/useReset.js
+++ src/hooks/common/useReset.js
@@ -1,6 +1,12 @@
 import {useEffect, useState} from "react"
 import {useDispatch, useSelector} from "react-redux"
-import {NEW_EMPTY_HORDER, NEW_EMPTY_ORDER, setCurrentHorder, setCurrentPreOrder} from "../../redux/ordersReducer.js"
+import {
+    NEW_EMPTY_HORDER,
+    NEW_EMPTY_ORDER,
+    setCurrentHorder,
+    setCurrentPreOrder,
+    setHorderPaying, setPreOrderPaying
+} from "../../redux/ordersReducer.js"
 
 export function useReset() {
 
@@ -10,6 +16,12 @@ export function useReset() {
     const uid_user = useSelector(state => state.auth.uid)
 
     const [clear, set_clear] = useState(0)
+
+    const pre_order = useSelector(state => state.orders.pre_order)
+    const horder = useSelector(state => state.orders.horder)
+
+    const pre_order_paying = useSelector(state => state.orders.pre_order_paying)
+    const horder_paying = useSelector(state => state.orders.horder_paying)
 
     // Очищаем по старым реквизитам данные
     useEffect(() => {
@@ -22,4 +34,12 @@ export function useReset() {
         dispatch(setCurrentHorder(NEW_EMPTY_HORDER()))
     }, [dispatch, clear])
 
+    useEffect(() => {
+        if (horder_paying) {
+            dispatch(setHorderPaying(false))
+        }
+        if (pre_order_paying) {
+            dispatch(setPreOrderPaying(false))
+        }
+    }, [pre_order.uid, horder.uid, pre_order.in_base, horder.in_base, dispatch])
 }
