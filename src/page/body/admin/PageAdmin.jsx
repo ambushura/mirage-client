4b4189413189cdfac5afffdcd119647b8f2c7fc3
ambushura <gumbertep@gmail.com>
2025-09-20 23:13:56 +0300

    update

--- src/page/body/admin/PageAdmin.jsx
+++ src/page/body/admin/PageAdmin.jsx
@@ -2,10 +2,8 @@ import {Box} from "@mui/material"
 import {useEffect, useState} from "react"
 import {useDispatch, useSelector} from "react-redux"
 import {NEW_EMPTY_HORDER, NEW_EMPTY_ORDER, setCurrentHorder, setCurrentPreOrder} from "../../../redux/ordersReducer.js"
-import AdminMenu from "../top-menu/AdminMenu.jsx"
 import OrdersCinema from "./orders/cinema/OrdersCinema.jsx"
 import OrdersHoreca from "./orders/horeca/OrdersHoreca.jsx"
-import Order from "../../right-panel/Order.jsx"
 import Zbooks from "./total/Zbooks.jsx"
 import Operations from "./total/Operations.jsx"
 import Egais from "./egais/EGAIS.jsx"
@@ -18,8 +16,6 @@ const PageAdmin = () => {
 
     const dispatch = useDispatch()
 
-    const pre_order = useSelector(state => state.orders.pre_order)
-    const horder = useSelector(state => state.orders.horder)
     const current_page = useSelector(state => state.interface.current_page)
     const [update_cinema, set_update_cinema] = useState(true)
     const [update_horeca, set_update_horeca] = useState(true)
@@ -29,24 +25,22 @@ const PageAdmin = () => {
         dispatch(setCurrentHorder(NEW_EMPTY_HORDER()))
     }, [dispatch])
 
-    return <Box id='content-box'>
-        <Box id='content-wrap'>
-            <Box id='content'>
-                <Box id="page-admin">
-                    {current_page === 'admin/orders/cinema' ? <OrdersCinema update_cinema={update_cinema}/> : null}
-                    {current_page === 'admin/orders/horeca' ? <OrdersHoreca update_horeca={update_horeca}/> : null}
-                    {current_page === 'admin/zbooks' ? <Zbooks/> : null}
-                    {current_page === 'admin/operations' ? <Operations/> : null}
-                    {current_page === 'admin/egais' ? <Egais/> : null}
-                    {current_page === 'admin/staff' ? <PageStaff/> : null}
-                    {current_page === 'admin/halls' ? <PageHalls/> : null}
-                    {current_page === 'admin/equipment' ? <PageEquipment/> : null}
-                    {current_page === 'admin/acquiring' ? <ZPinpads/> : null}
-                </Box>
+    return <Box id='content-box' sx={{overflowY: 'auto'}}>
+        <Box sx={{display: 'flex', flexDirection: 'column'}}>
+            <Box id='content-header'></Box>
+            <Box id='content' sx={{padding: '10px 0'}}>
+                {current_page === 'admin/orders/cinema' ? <OrdersCinema update_cinema={update_cinema}/> : null}
+                {current_page === 'admin/orders/horeca' ? <OrdersHoreca update_horeca={update_horeca}/> : null}
+                {current_page === 'admin/zbooks' ? <Zbooks/> : null}
+                {current_page === 'admin/operations' ? <Operations/> : null}
+                {current_page === 'admin/egais' ? <Egais/> : null}
+                {current_page === 'admin/staff' ? <PageStaff/> : null}
+                {current_page === 'admin/halls' ? <PageHalls/> : null}
+                {current_page === 'admin/equipment' ? <PageEquipment/> : null}
+                {current_page === 'admin/acquiring' ? <ZPinpads/> : null}
             </Box>
+            <Box id='content-footer'></Box>
         </Box>
-        {(current_page === 'admin/orders/cinema' || current_page === 'admin/orders/horeca') && (pre_order.in_base || horder.in_base) ?
-            <Order/> : null}
     </Box>
 }
 
