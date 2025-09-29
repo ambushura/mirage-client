c8ee9816a625cc96a39c00d62b94cbc63762ac4b
ambushura <gumbertep@gmail.com>
2025-09-29 14:46:56 +0300

    update

--- src/page/body/admin/total/ZPinpads.jsx
+++ src/page/body/admin/total/ZPinpads.jsx
@@ -5,7 +5,7 @@ import {common_documents_pinpads_get, common_documents_slips_get} from "../../..
 import {Box} from "@mui/material"
 import {cleanSlips, cleanZPinpads, setSlips, setZPinpads} from "../../../../redux/documentsReducer.js"
 import {DataGridPro} from "@mui/x-data-grid-pro"
-import dayjs from "dayjs";
+import dayjs from "dayjs"
 
 const ZPinpads = () => {
 
@@ -132,72 +132,61 @@ const ZPinpads = () => {
 
 export default ZPinpads
 
-export const columns_zpinpads = [
-    {field: 'id', headerName: '', width: 10},
-    {field: 'name_organization', headerName: 'Организация', width: 100},
-    {field: 'inn', headerName: 'ИНН', width: 100},
-    {field: 'pinpad_number', headerName: 'ID пинпада', width: 100},
-    {field: 'date_shift', headerName: 'Смена', width: 90},
-    {field: 'slip_15', headerName: '15', type: 'number', width: 100}, {
-        field: 'slip_19',
-        headerName: '19',
-        type: 'number',
-        width: 100
-    }, {
-        field: 'slip_25', headerName: '25', type: 'number', width: 100
-    }, {field: 'slip_39', headerName: '39', type: 'number', width: 100}, {
-        field: 'slip_65', headerName: '65', type: 'number', width: 100
-    }, {field: 'slip_67', headerName: '67', type: 'number', width: 100}, {
-        field: 'slip_90', headerName: '90', width: 100
-    }, {field: 'type', headerName: 'Т', type: 'number', width: 100},]
+export const columns_zpinpads = [{field: 'id', headerName: '', width: 10}, {
+    field: 'name_organization', headerName: 'Организация', width: 100
+}, {field: 'inn', headerName: 'ИНН', width: 100}, {
+    field: 'pinpad_number', headerName: 'ID пинпада', width: 100
+}, {field: 'date_shift', headerName: 'Смена', width: 90}, {
+    field: 'slip_15', headerName: '15', type: 'number', width: 100
+}, {
+    field: 'slip_19', headerName: '19', type: 'number', width: 100
+}, {
+    field: 'slip_25', headerName: '25', type: 'number', width: 100
+}, {field: 'slip_39', headerName: '39', type: 'number', width: 100}, {
+    field: 'slip_65', headerName: '65', type: 'number', width: 100
+}, {field: 'slip_67', headerName: '67', type: 'number', width: 100}, {
+    field: 'slip_90', headerName: '90', width: 100
+}, {field: 'type', headerName: 'Т', type: 'number', width: 100},]
 
-export const columns_slips = [
-    {field: 'id', headerName: '', width: 10},
-    {
-        field: 'date_shift', headerName: 'Дата смены', width: 100, type: 'date', valueGetter: (param) => {
-            return param ? dayjs(param).toDate() : null
-        }
-    },
-    {
-        field: 'date_create', headerName: 'Создан', width: 140, type: 'dateTime', valueGetter: (param) => {
-            return param ? dayjs(param).toDate() : null
-        }
-    },
-    {field: 'pinpad_number', headerName: 'ID', width: 100},
+export const columns_slips = [{field: 'id', headerName: '', width: 10}, {
+    field: 'date_shift', headerName: 'Дата смены', width: 100, type: 'date', valueGetter: (param) => {
+        return param ? dayjs(param).toDate() : null
+    }
+}, {
+    field: 'date_create', headerName: 'Создан', width: 140, type: 'dateTime', valueGetter: (param) => {
+        return param ? dayjs(param).toDate() : null
+    }
+}, {field: 'pinpad_number', headerName: 'ID', width: 100},
 
-    {field: 'inn', headerName: 'ИНН', width: 100},
-    {field: 'name_creator', headerName: 'Автор', width: 100},
-    {field: 'name_organization', headerName: 'Организация', width: 100},
-    {field: 'print_error', headerName: 'Ошибка печати', width: 100},
-    {field: 'slip0', headerName: '0', width: 100},
-    {field: 'slip4', headerName: '4', width: 100},
-    {
+    {field: 'inn', headerName: 'ИНН', width: 100}, {
+        field: 'name_creator', headerName: 'Автор', width: 100
+    }, {field: 'name_organization', headerName: 'Организация', width: 100}, {
+        field: 'print_error', headerName: 'Ошибка печати', width: 100
+    }, {field: 'slip0', headerName: '0', width: 100}, {field: 'slip4', headerName: '4', width: 100}, {
         field: 'slip6', headerName: '6', width: 100, type: 'dateTime', valueGetter: (param) => {
             return param ? dayjs(param).toDate() : null
         }
-    },
-    {field: 'slip9', headerName: '9', width: 100},
-    {field: 'slip10', headerName: '10', width: 100},
-    {field: 'slip11', headerName: '11', width: 100},
-    {field: 'slip13', headerName: '13', width: 100},
-    {field: 'slip14', headerName: '14', width: 100},
-    {field: 'slip15', headerName: '15', width: 100},
-    {field: 'slip19', headerName: '19', width: 100},
-    {field: 'slip21', headerName: '21', width: 100},
-    {field: 'slip23', headerName: '23', width: 100},
-    {field: 'slip25', headerName: '25', width: 100},
-    {field: 'slip26', headerName: '26', width: 100},
-    {field: 'slip27', headerName: '27', width: 100},
-    {field: 'slip28', headerName: '28', width: 100},
-    {field: 'slip39', headerName: '39', width: 100},
-    {field: 'slip86', headerName: '86', width: 100},
-    {field: 'uid_creator', headerName: '', width: 100},
-    {field: 'uid_order_cinema', headerName: '', width: 100},
-    {field: 'uid_order_food', headerName: '', width: 100},
-    {field: 'uid_pinpad', headerName: '', width: 100},
-    {field: 'slip90', headerName: '90', width: 100},
-    {field: 'printed', headerName: 'Напечатан', width: 100, type: 'boolean'},
-    {
+    }, {field: 'slip9', headerName: '9', width: 100}, {field: 'slip10', headerName: '10', width: 100}, {
+        field: 'slip11', headerName: '11', width: 100
+    }, {field: 'slip13', headerName: '13', width: 100}, {
+        field: 'slip14', headerName: '14', width: 100
+    }, {field: 'slip15', headerName: '15', width: 100}, {
+        field: 'slip19', headerName: '19', width: 100
+    }, {field: 'slip21', headerName: '21', width: 100}, {
+        field: 'slip23', headerName: '23', width: 100
+    }, {field: 'slip25', headerName: '25', width: 100}, {
+        field: 'slip26', headerName: '26', width: 100
+    }, {field: 'slip27', headerName: '27', width: 100}, {
+        field: 'slip28', headerName: '28', width: 100
+    }, {field: 'slip39', headerName: '39', width: 100}, {
+        field: 'slip86', headerName: '86', width: 100
+    }, {field: 'uid_creator', headerName: '', width: 100}, {
+        field: 'uid_order_cinema', headerName: '', width: 100
+    }, {field: 'uid_order_food', headerName: '', width: 100}, {
+        field: 'uid_pinpad', headerName: '', width: 100
+    }, {field: 'slip90', headerName: '90', width: 100}, {
+        field: 'printed', headerName: 'Напечатан', width: 100, type: 'boolean'
+    }, {
         field: 'slip_type', headerName: 'Тип', width: 140, valueGetter: (param) => {
             switch (param) {
                 case 1:
@@ -206,6 +195,4 @@ export const columns_slips = [
                     return 'ОТМЕНА/ВОЗВРАТ'
             }
         }
-    },
-    {field: 'slip_sum', headerName: 'Сумма', width: 100},
-]
+    }, {field: 'slip_sum', headerName: 'Сумма', width: 100},]
